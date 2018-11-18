cc390951ecb80338c2e2bfea1211bfd51151830f
pbw <quddnr0708@gmail.com>
2018-11-18 17:40:45 +0900

    add project

---
+++ Project/movie/해외/movie.js
@@ -0,0 +1,9 @@
+﻿function openWin(src)
+{
+	win = window.open(""+src+"","win","width=350 height=540 directories=no")
+	win.document.write("<html><head>")
+	win.document.write("<title>포스터 확대보기</title>")
+	win.document.write("</head><body leftmargin=0 topmargin=0 onclick='self.close()'>")
+	win.document.write("<img src='"+src+"' width=350 height=540>")
+	win.document.write("</body></html>")
+}
